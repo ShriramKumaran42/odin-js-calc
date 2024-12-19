f6a79d09d004d2feef2457cb727c3472b6a7addf
ShriramKumaran42 <shriramkumaran142000@gmail.com>
2024-12-20 01:44:44 +0530

    finished the js part
    should change the css to look like a claculator

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
+
+
 let num1 = "";
 let operator = "";
 let num2 = "";
 
-function add(num1, num2){
-    return num1 + num2;
-}
-function sub(num1, num2){
-    return num1 - num2;
-}
-function multi(num1,num2){
-    return num1*num2;
-}
-function divi(num1,num2){
-    if(num2 ===0){
-        return "Error: cannot be zero"    
-    }
-    return num1/num2;
-}
-function modulo(num1,num2){
-    return num1%num2;
-}
 
-function operate(operator, num1, num2){
-    if(operator === "+"){
-        return add(num1,num2);
-    }
-    else if(operator === "-"){
-        return sub(num1,num2);
-    }
-    else if(operator === "*"){
-        return multi(num1,num2);
-    }
-    else if(operator === "/"){
-        return divi(num1,num2);
-    }
-    else if(operator === "%"){
-        return modulo(num1,num2);
-    }
-    else{
-        return "Invalid";
-    }
-}
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// function sub(num1, num2){
+//     return num1 - num2;
+// }
+// function multi(num1,num2){
+//     return num1*num2;
+// }
+// function divi(num1,num2){
+//     if(num2 ===0){
+//         return "Error: cannot be zero"    
+//     }
+//     return num1/num2;
+// }
+// function modulo(num1,num2){
+//     return num1%num2;
+// }
+
+
 
 
 
@@ -51,24 +35,79 @@ function operate(operator, num1, num2){
 const display = document.querySelector("#disp");
 
 document.querySelector(".numPad").addEventListener("click", function(event) {
-    if(event.target.classList.contains("nums")){
+    const button =event.target;
+    if(button.classList.contains("nums")){
         if(!operator){
-            num1 += event.target.textContent;
-            display.value += num1;
+            num1 += button.textContent;
+            display.value = num1;
         }
         else{
-            num2 += event.target.textContent;
-            display.value += num2;
+            num2 += button.textContent;
+            display.value = num2;
+        }
+    }
+    if(button.classList.contains("operator")){
+        
+
+        if(num1 && num2){
+            operator = button.textContent;
+            const resultDisp = operate(operator, parseFloat(num1), parseFloat(num2));
+            display.value = resultDisp;
+            num1 = resultDisp.toString();
+            operator = button.textContent;
+            num2 = "";           
+        }
+        else if(num1){
+            operator = button.textContent;
+            display.value = num1;
+        }
+    }
+    if(button.id === "equalTo"){
+        if(num1 && operator && num2){
+            const resultDisp = operate(operator, parseFloat(num1), parseFloat(num2));
+            display.value = resultDisp;
+    
+            num1 = resultDisp.toString();
+            operator = "";
+            num2 = "";
+        }
+    }
+    if(button.id === "allClear"){
+        num1 = "";
+        operator = "";
+        num2 = "";
+        display.value = "";
+    }
+    if(button.id === "pOrM"){
+        if(!operator){
+            num1 = (-parseFloat(num1)).toString();
+            display.value = num1;
+        }
+        else if(num2){
+            num2 = (-parseFloat(num2)).toString();
+            display.value = num2;
         }
     }
 });
 
-document.querySelector(".numPad").addEventListener("click", function(event) {
-    if(event.target.classList.contains("operator")){
-        if(num1){
-            operator = event.target.textContent;
-            display.value = operator;
-        }
-    }
-})
 
+function operate(operator, num1, num2){
+    if(operator === "+"){
+        return num1 + num2;
+    }
+    else if(operator === "-"){
+        return num1 - num2;
+    }
+    else if(operator === "X"){
+        return num1*num2;
+    }
+    else if(operator === "/"){
+        return num2===0 ? "by 0... Really" : (num1/num2).toFixed(2);
+    }
+    else if(operator === "%"){
+        return num2 ===0 ? "by 0... Really" : (num1%num2).toFixed(2);
+    }
+    else{
+        return "Invalid";
+    }
+}
